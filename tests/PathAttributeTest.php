2ff8d9d67eb2da95cfcc2a5a3767e2fef1353f13
Lexgur <edgaras.malukas@gmail.com>
2025-03-31 20:23:44 +0300

    Path attribute tested

---
+++ tests/PathAttributeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Lexgur\GondorGains\Attribute\Path;
+use PHPUnit\Framework\TestCase;
+
+class PathAttributeTest extends TestCase
+{
+    public function testGetPath(): void
+    {
+        $class = new ReflectionClass(SomethingCreateController::class);
+        $attributes = $class->getAttributes(Path::class);
+        $this->assertEquals('/something/create', $attributes[0]->newInstance()->getPath());
+    }
+}
+
+#[Path('/something/create')]
+class SomethingCreateController
+{
+}
