59bd3c422ac3781c73f9eedd024ababf7c3d0205
Lexgur <edgaras.malukas@gmail.com>
2025-04-01 09:07:27 +0300

    declared strict types in connection test

--- src/Connection.php
+++ src/Connection.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Lexgur\GondorGains;
 
 use PDO;
-use PDOException;
 
 class Connection
 {
--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Lexgur\GondorGains\Connection;
 use PHPUnit\Framework\TestCase;
 
