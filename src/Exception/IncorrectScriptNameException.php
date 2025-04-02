e56d736cfee9fe8dab379ff315bc3589a4788391
Lexgur <edgaras.malukas@gmail.com>
2025-04-02 19:53:15 +0300

    Exceptions fixed according to Php-cs-fixer

--- src/Exception/IncorrectScriptNameException.php
+++ src/Exception/IncorrectScriptNameException.php
@@ -4,7 +4,4 @@ declare(strict_types=1);
 
 namespace Lexgur\GondorGains\Exception;
 
-class IncorrectScriptNameException extends \Exception
-{
-
-}
+class IncorrectScriptNameException extends \Exception {}
