47a1b49577a7a9d7f259c40790fca10eb1830615
Lexgur <edgaras.malukas@gmail.com>
2025-04-02 19:51:03 +0300

    ScriptTest and ScriptValidationTest has been added.

---
+++ tests/ScriptNameValidationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Lexgur\GondorGains\Exception\IncorrectScriptNameException;
+use Lexgur\GondorGains\Validation\ScriptNameValidation;
+use PHPUnit\Framework\TestCase;
+
+class ScriptNameValidationTest extends TestCase
+{
+    private ScriptNameValidation $validation;
+
+    protected function setUp(): void
+    {
+        $this->validation = new ScriptNameValidation();
+    }
+
+    public function testSuccessfulValidation(): void
+    {
+        $scriptClass = 'Lexgur/GondorGains/Script/HelloWorldScript';
+        $this->validation->validate($scriptClass);
+
+        $this->assertTrue(true);
+    }
+
+    public function testValidationWithIncorrectScriptStartThrowsIncorrectScriptNameException(): void
+    {
+        $this->expectException(IncorrectScriptNameException::class);
+        $this->expectExceptionMessage('Script should start with Lexgur/GondorGains/Script/');
+
+        $scriptClass = '//Lexgur/GondorGains/Script/';
+        $this->validation->validate($scriptClass);
+    }
+
+    public function testValidationWithoutSpecifiedScriptThrowsIncorrectScriptNameException(): void
+    {
+        $this->expectException(IncorrectScriptNameException::class);
+        $this->expectExceptionMessage('Script name must include a valid class after Lexgur/GondorGains/Script/');
+
+        $scriptClass = 'Lexgur/GondorGains/Script/';
+        $this->validation->validate($scriptClass);
+    }
+}
--- tests/ScriptTest.php
+++ tests/ScriptTest.php
@@ -2,18 +2,55 @@
 
 declare(strict_types=1);
 
+use Lexgur\GondorGains\Exception\ScriptFailedToRunException;
 use Lexgur\GondorGains\Script;
 use PHPUnit\Framework\TestCase;
 
 class ScriptTest extends TestCase
 {
-    private Script $script;
-
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->tmpTestDir = __DIR__ . '/../tmp/tests/';
+    }
+
+    public function testRunValidScript(): void
+    {
+        $scriptClass = 'Lexgur/GondorGains/Script/TempTestScript';
+        $filePath = $this->tmpTestDir . 'TempTestScript.php';
+
+        $namespace = "Lexgur\\GondorGains\\Script";
+        $className = "TempTestScript";
+
+        $scriptContent = <<<PHP
+        <?php
+        namespace $namespace;
+
+        use Lexgur\GondorGains\Script\ScriptInterface;
+
+        class $className implements ScriptInterface {
+            public function run(): int {
+                return 1;
+            }
+        }
+        PHP;
+
+        file_put_contents($filePath, $scriptContent);
+
+        require_once $filePath;
+        $script = new Script();
+        $result = $script->run($scriptClass);
+
+        $this->assertSame(1, $result);
+    }
+
+    public function testRunInvalidScriptThrowsScriptFailedToRunException(): void
+    {
+        $this->expectException(ScriptFailedToRunException::class);
+
+        $script = new Script();
+        $script->run('Lexgur/GondorGains/Script/NonExistentScript');
     }
 
     protected function tearDown(): void
