6b07b8ba79b5fdd85b6c483341dc8fac6fe1211a
Lexgur <edgaras.malukas@gmail.com>
2025-04-02 12:47:44 +0300

    fix: getScripts was an incorrect approach

--- src/Script.php
+++ src/Script.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace Lexgur\GondorGains;
 
-use Lexgur\GondorGains\Exception\ScriptFailedToRunException;
-use Lexgur\GondorGains\Script\ScriptInterface;
-use ReflectionClass;
-use Throwable;
-
 class Script {
 
     private Container $container;
@@ -18,27 +13,9 @@ class Script {
         $this->container = new Container();
     }
 
-    public function getScripts(): array
+    public function run(): void
     {
-        $scriptPath = __DIR__ . '/Script';
 
-        $scripts = [];
-        foreach(scandir($scriptPath) as $scriptFile){
-            try {
-                if (pathinfo($scriptFile, PATHINFO_EXTENSION) === 'php'){
-                    $className = "Lexgur\\GondorGains\\Script\\" . pathinfo($scriptFile, PATHINFO_FILENAME);
-                } if (class_exists($className)){
-                  $reflection = new ReflectionClass($className);
-                    if ($reflection->implementsInterface(ScriptInterface::class)){
-                        $scripts[$className] = $this->container->get($className);
-                    }
-                }
-
-            } catch (Throwable $e) {
-                throw new ScriptFailedToRunException('Script loading failed: ' . $e->getMessage());
-            }
-        }
-        return $scripts;
     }
 
 }
