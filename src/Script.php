ad6260e6c9da5ac344a54ebdc1ef7df6fd9ebd57
Lexgur <edgaras.malukas@gmail.com>
2025-04-02 14:58:26 +0300

    Now the script works as well as command such as php bin/script scriptClass

--- src/Script.php
+++ src/Script.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace Lexgur\GondorGains;
 
+use Lexgur\GondorGains\Exception\ScriptFailedToRunException;
+use Lexgur\GondorGains\Script\ScriptInterface;
+
 class Script {
 
-    public function run(): void
+    protected Container $container;
+
+    public function __construct()
     {
-        echo 'Hello world';
+        $this->container = new Container();
+    }
+
+    public function run(string $scriptClass): void
+    {
+        $className = '\\' . str_replace('/', '\\', $scriptClass);
+        $service = $this->container->get($className);
+
+        if (!$service instanceof ScriptInterface) {
+            throw new ScriptFailedToRunException('Script does not belong to ScriptInterface');
+        }
+        echo $service->run();
     }
 }
