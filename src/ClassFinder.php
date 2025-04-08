ac5acdea9b8b6678cb3f380d4ae075d332cf3005
Lexgur <edgaras.malukas@gmail.com>
2025-04-08 10:26:32 +0300

    fix: removed exceptions from ClassFinder as they were not needed.

--- src/ClassFinder.php
+++ src/ClassFinder.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Lexgur\GondorGains;
 
-use Lexgur\GondorGains\Exception\ClassFinderFailedToRunException;
-use Lexgur\GondorGains\Exception\FilePathReadException;
-use Lexgur\GondorGains\Exception\IncorrectRoutePathException;
+use ReflectionException;
 
 class ClassFinder
 {
@@ -38,7 +36,7 @@ class ClassFinder
     {
         return $this->processPhpFiles(function (\ReflectionClass $reflectionClass) use ($namespace) : bool {
             $classNamespace = $reflectionClass->getNamespaceName();
-            return $classNamespace === $namespace || str_starts_with($classNamespace, $namespace);
+            return str_starts_with($classNamespace, $namespace);
         });
     }
 
@@ -59,7 +57,7 @@ class ClassFinder
     {
         $content = file_get_contents($path);
         if (empty($content)) {
-            throw new FilePathReadException("Failed to read file: {$path}");
+            return null;
         }
 
         $namespace = null;
@@ -72,35 +70,25 @@ class ClassFinder
             return $namespace ? $namespace . '\\' . $className : $className;
         }
 
-        throw new IncorrectRoutePathException('Class not found: '.$path);
+        return null;
     }
 
-    /** @return list<string> */
+    /** @return list<string>
+     * @throws ReflectionException
+     */
     private function processPhpFiles(callable $condition): array
     {
         $phpFiles = $this->getPhpFiles();
         $results = [];
 
         foreach ($phpFiles as $file) {
-            try {
                 $path = $file->getPathname();
                 $className = $this->getFullClassName($path);
-
-                if (!$className) {
-                    continue;
-                }
-
                 $reflectionClass = new \ReflectionClass($className);
 
                 if ($condition($reflectionClass, $className)) {
                     $results[] = $className;
                 }
-
-            } catch (\Throwable $e) {
-                throw new ClassFinderFailedToRunException(
-                    'Error while scanning file "' . $file . '": ' . $e->getMessage()
-                );
-            }
         }
         return $results;
     }
--- src/Exception/ClassFinderFailedToRunException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Lexgur\GondorGains\Exception;
-
-class ClassFinderFailedToRunException extends \RuntimeException {}
