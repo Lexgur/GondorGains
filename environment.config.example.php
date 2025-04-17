b72ec54ce93e7603a3552924ea36f6519d77a140
Lexgur <edgaras.malukas@gmail.com>
2025-04-17 14:25:30 +0300

    config example now adjusted to be used for dev env.

--- environment.config.example.php
+++ environment.config.example.php
@@ -4,4 +4,7 @@ declare(strict_types=1);
 
 return [
     'dsn' => 'sqlite:' . __DIR__ . '/tmp/dev/GondorGains.sqlite',
+    'directory' => __DIR__ . '/src/Migration',
+    'migratedRegistryPath' => __DIR__ . '/tmp/dev/migrations.json',
+    'controllerDir' => __DIR__ . '/src/Controller',
 ];
