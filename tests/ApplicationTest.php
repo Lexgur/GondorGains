4eaf1c33e6ce56ff660b46aabc78bb68952be0d8
Lexgur <edgaras.malukas@gmail.com>
2025-04-17 15:19:30 +0300

    Renamed the test so that it phpunit works properly.

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -4,46 +4,8 @@ declare(strict_types=1);
 
 namespace Lexgur\GondorGains\Tests;
 
-use Lexgur\GondorGains\Application;
-use PHPUnit\Framework\TestCase;
-use Lexgur\GondorGains\Container;
-use Lexgur\GondorGains\Router;
-
-class ApplicationTest extends TestCase
+class ApplicationTest extends WebTestCase
 {
-    private Application $app;
-
-    private Router $router;
-
-    private Container $container;
-
-    protected function setUp(): void
-    {
-        $config = require __DIR__ . '/../config.php';
-        $this->container = new Container($config);
-        $this->router = $this->container->get(Router::class);
-        $this->app = $this->container->get(Application::class);
-
-        $this->app->run();
-        $this->router->registerControllers();
-    }
-
-    private function request(string $method, string $url, array $data = []): string
-    {
-        $_SERVER['REQUEST_METHOD'] = strtoupper($method);
-        $_SERVER['REQUEST_URI'] = $url;
-
-        if ($method === 'POST') {
-            $_POST = $data;
-        } else {
-            $_GET = $data;
-        }
-
-        ob_start();
-        $this->app->run();
-        return ob_get_clean();
-    }
-
     public function testBadRequestException(): void
     {
         $output = $this->request('GET', '/test/bad-request');
@@ -76,4 +38,3 @@ class ApplicationTest extends TestCase
         $this->assertStringContainsString('404', $output);
     }
 }
-
---
+++ tests/WebTestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lexgur\GondorGains\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Lexgur\GondorGains\Application;
+
+abstract class WebTestCase extends TestCase
+{
+    public function request(string $method, string $url, array $data = []): string
+    {
+        $app = new Application();
+        
+        $_SERVER['REQUEST_METHOD'] = strtoupper($method);
+        $_SERVER['REQUEST_URI'] = $url;
+
+        if ($method === 'POST') {
+            $_POST = $data;
+        } else {
+            $_GET = $data;
+        }
+
+        ob_start();
+        $app->run();
+        return ob_get_clean();
+    }
+}
