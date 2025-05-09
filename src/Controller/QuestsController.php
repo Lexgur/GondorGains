0bc332f26fb9426a0ecd5cba6bfb24bb3794ec88
Lexgur <edgaras.malukas@gmail.com>
2025-05-09 18:45:51 +0300

    Adjusted mistakes in Quests and Weakling controllers.

--- src/Controller/QuestsController.php
+++ src/Controller/QuestsController.php
@@ -7,6 +7,7 @@ namespace Lexgur\GondorGains\Controller;
 use Lexgur\GondorGains\Attribute\Path;
 use Lexgur\GondorGains\Exception\ForbiddenException;
 use Lexgur\GondorGains\Service\RandomQuote;
+use Lexgur\GondorGains\Service\Session;
 use Lexgur\GondorGains\TemplateProvider;
 
 #[Path('/quests')]
@@ -14,18 +15,18 @@ class QuestsController extends AbstractController
 {
     private RandomQuote $randomQuote;
 
-    public function __construct(TemplateProvider $templateProvider, RandomQuote $randomQuote)
+    private Session $session;
+
+    public function __construct(TemplateProvider $templateProvider, RandomQuote $randomQuote, Session $session)
     {
         parent::__construct($templateProvider);
         $this->randomQuote = $randomQuote;
+        $this->session = $session;
     }
 
     public function __invoke(): string
     {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-        if (!isset($_SESSION['id'])) {
+        if (!$this->session->hasStarted()) {
             throw new ForbiddenException();
         }
         return $this->render("quests.html.twig", [
