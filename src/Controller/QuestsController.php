f99ff316c77fc693c25fae00534614566194bc17
Lexgur <edgaras.malukas@gmail.com>
2025-05-11 12:04:25 +0300

    Both controllers adjusted, htmls as well to adjust and fit in with the diagram.

--- src/Controller/QuestsController.php
+++ src/Controller/QuestsController.php
@@ -6,6 +6,7 @@ namespace Lexgur\GondorGains\Controller;
 
 use Lexgur\GondorGains\Attribute\Path;
 use Lexgur\GondorGains\Exception\ForbiddenException;
+use Lexgur\GondorGains\Repository\ChallengeModelRepository;
 use Lexgur\GondorGains\Service\CurrentUser;
 use Lexgur\GondorGains\Service\RandomQuote;
 use Lexgur\GondorGains\TemplateProvider;
@@ -17,11 +18,14 @@ class QuestsController extends AbstractController
 
     private CurrentUser $currentUser;
 
-    public function __construct(TemplateProvider $templateProvider, RandomQuote $randomQuote, CurrentUser $currentUser)
+    private ChallengeModelRepository $challengeRepository;
+
+    public function __construct(TemplateProvider $templateProvider, RandomQuote $randomQuote, CurrentUser $currentUser, ChallengeModelRepository $challengeRepository)
     {
         parent::__construct($templateProvider);
         $this->randomQuote = $randomQuote;
         $this->currentUser = $currentUser;
+        $this->challengeRepository = $challengeRepository;
     }
 
     public function __invoke(): string
@@ -29,8 +33,24 @@ class QuestsController extends AbstractController
         if ($this->currentUser->isAnonymous()) {
             throw new ForbiddenException();
         }
+        $userChallenges = $this->challengeRepository->fetchAllChallenges();
+        $completedChallenges = [];
+
+        foreach ($userChallenges as $userChallenge) {
+            if ($userChallenge->getCompletedAt() !== null) {
+                $completedChallenges[] = [
+                    'id' => $userChallenge->getChallengeId(),
+                    'started_at' => $userChallenge->getStartedAt()->format('Y-m-d H:i'),
+                    'completed_at' => $userChallenge->getCompletedAt()->format('Y-m-d H:i'),
+                ];
+            }
+        }
+
         return $this->render("quests.html.twig", [
             'quote' => $this->randomQuote->getQuote(),
+            'message' => 'List of completed quests, Gondorian:',
+            'challenges' => $completedChallenges,
+            'quest' => '/daily-quest/start',
         ]);
     }
 }
--- src/Controller/WeaklingController.php
+++ src/Controller/WeaklingController.php
@@ -31,6 +31,8 @@ class WeaklingController extends AbstractController
         }
         return $this->render("weakling.html.twig", [
             'quote' => $this->randomQuote->getQuote(),
+            'message' => "You have not completed a single quest? I know a man in white robes, that would be disappointed",
+            'quest' => '/daily-quest/start'
         ]);
     }
 }
