9b80feb424504bfbdf88ab7dd0461d4f4cfcdbca
Lexgur <edgaras.malukas@gmail.com>
2025-05-18 21:28:04 +0300

    A much more simple way to generateChallenge has been tested and implemented.

--- src/Service/ChallengeCreatorService.php
+++ src/Service/ChallengeCreatorService.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace Lexgur\GondorGains\Service;
 
+use Lexgur\GondorGains\Exception\ExerciseNotFoundException;
+use Lexgur\GondorGains\Model\Exercise;
+use Random\RandomException;
+
 class ChallengeCreatorService
 {
+    public function __construct(private RandomExerciseFetcher $fetcher) {}
+
+    /**
+     * @return Exercise[]|null[]
+     * @throws ExerciseNotFoundException
+     * @throws RandomException
+     */
+    public function generateChallenge(?int $rotation = null): array
+    {
+        return $this->fetcher->fetchRandomExercise($rotation);
+    }
 }
--- tests/ChallengeCreatorServiceTest.php
+++ tests/ChallengeCreatorServiceTest.php
@@ -1,19 +1,85 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Lexgur\GondorGains\Tests;
 
-use Lexgur\GondorGains\Repository\ChallengeModelRepository;
-use Lexgur\GondorGains\Repository\ExerciseModelRepository;
+use Lexgur\GondorGains\Exception\ExerciseNotFoundException;
+use Lexgur\GondorGains\Model\Exercise;
 use Lexgur\GondorGains\Service\ChallengeCreatorService;
 use Lexgur\GondorGains\Service\RandomExerciseFetcher;
-use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Random\RandomException;
 
 class ChallengeCreatorServiceTest extends TestCase
 {
+    private RandomExerciseFetcher&MockObject $fetcher;
+    private ChallengeCreatorService $service;
 
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $this->fetcher = $this->createMock(RandomExerciseFetcher::class);
+        $this->service = new ChallengeCreatorService($this->fetcher);
+    }
+
+    /**
+     * @throws ExerciseNotFoundException|RandomException|Exception
+     */
+    public function testGenerateChallengeReturnsExercisesForSpecifiedRotation(): void
+    {
+        $rotation = 2;
+        $expectedExercises = [
+            $this->createStub(Exercise::class),
+            $this->createStub(Exercise::class),
+            $this->createStub(Exercise::class)
+        ];
+
+        $this->fetcher->expects($this->once())
+            ->method('fetchRandomExercise')
+            ->with($rotation)
+            ->willReturn($expectedExercises);
+
+        $result = $this->service->generateChallenge($rotation);
+
+        $this->assertSame($expectedExercises, $result);
+        $this->assertCount(3, $result);
+    }
+
+    /**
+     * @throws ExerciseNotFoundException|RandomException|Exception
+     */
+    public function testGenerateChallengeUsesNextRotationWhenNoneSpecified(): void
+    {
+        $expectedExercises = [
+            $this->createStub(Exercise::class),
+            $this->createStub(Exercise::class)
+        ];
+
+        $this->fetcher->expects($this->once())
+            ->method('fetchRandomExercise')
+            ->with(null)
+            ->willReturn($expectedExercises);
+
+        $result = $this->service->generateChallenge();
+
+        $this->assertSame($expectedExercises, $result);
+        $this->assertCount(2, $result);
+    }
+
+    /**
+     * @throws ExerciseNotFoundException|RandomException
+     */
+    public function testGenerateChallengePropagatesExceptions(): void
+    {
+        $this->expectException(ExerciseNotFoundException::class);
+
+        $this->fetcher->expects($this->once())
+            ->method('fetchRandomExercise')
+            ->willThrowException(new ExerciseNotFoundException());
+        $this->service->generateChallenge();
+    }
 }
