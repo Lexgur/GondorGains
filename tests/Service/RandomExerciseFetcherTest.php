1bff9c97fd3e627e73863cffc3528dfb3ee2196a
edgaras <edgaras.malukas@gmail.com>
2025-06-03 15:51:56 +0300

    Test is now using actual methods and classes without any mocking or stubbing

--- tests/Service/RandomExerciseFetcherTest.php
+++ tests/Service/RandomExerciseFetcherTest.php
@@ -2,96 +2,63 @@
 
 namespace Lexgur\GondorGains\Tests\Service;
 
-use InvalidArgumentException;
+use Lexgur\GondorGains\Connection;
+use Lexgur\GondorGains\Container;
+use Lexgur\GondorGains\Exception\CircularDependencyException;
 use Lexgur\GondorGains\Exception\NotEnoughExercisesException;
 use Lexgur\GondorGains\Model\Exercise;
 use Lexgur\GondorGains\Model\MuscleGroup;
 use Lexgur\GondorGains\Repository\ExerciseModelRepository;
+use Lexgur\GondorGains\Script\RunMigrationsScript;
 use Lexgur\GondorGains\Service\RandomExerciseFetcher;
 use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\MockObject\Exception;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Random\RandomException;
 
 class RandomExerciseFetcherTest extends TestCase
 {
-    private const MIN_EXERCISES_ROTATION_1 = 4;
-    private const MAX_EXERCISES_ROTATION_1 = 6;
-    private const MIN_EXERCISES_ROTATION_2 = 8;
-    private const MAX_EXERCISES_ROTATION_2 = 12;
-
+    private ExerciseModelRepository $repository;
     private RandomExerciseFetcher $exerciseFetcher;
 
-    /** @var ExerciseModelRepository&MockObject */
-    private ExerciseModelRepository $repository;
-
     /**
-     * @throws Exception
+     * @throws CircularDependencyException
      */
     protected function setUp(): void
     {
-        $this->repository = $this->createMock(ExerciseModelRepository::class);
+        $config = require __DIR__.'/../../config.php';
+        $container = new Container($config);
+
+        $runMigrations = $container->get(RunMigrationsScript::class);
+        $runMigrations->run();
+
+        $connection = $container->get(Connection::class);
+        $connection->connect()->exec('DELETE FROM exercises');
+
+        $this->repository = $container->get(ExerciseModelRepository::class);
         $this->exerciseFetcher = new RandomExerciseFetcher($this->repository);
     }
 
     /**
-     * Creates test exercises for a specific muscle group
-     * @return array<Exercise>
-     */
-    private function createTestExercises(MuscleGroup $muscleGroup, int $count): array
-    {
-        $exercises = [];
-        for ($i = 1; $i <= $count; $i++) {
-            $exercises[] = new Exercise(
-                "Exercise $i",
-                $muscleGroup,
-                "Description $i",
-                $i
-            );
-        }
-        return $exercises;
-    }
-
-    /**
-     * @param int $muscleGroupRotation
-     * @param int $minExercises
-     * @param int $maxExercises
-     * @return void
      * @throws RandomException
      */
-    #[DataProvider('provideValidRotations')]
-    public function testShouldReturnValidNumberOfExercisesForRotation(int $muscleGroupRotation, int $minExercises, int $maxExercises): void
+    #[DataProvider('provideTestShouldReturnValidNumberOfExercisesForRotation')]
+    public function testShouldReturnValidNumberOfExercisesForRotation(int $rotation, int $min, int $max): void
     {
-        $this->repository
-            ->method('fetchByMuscleGroup')
-            ->willReturn($this->createTestExercises(MuscleGroup::CHEST, 3));
+        $this->seedExercises();
 
-        $exerciseIds = $this->exerciseFetcher->fetchRandomExercise($muscleGroupRotation);
+        $result = $this->exerciseFetcher->fetchRandomExercise($rotation);
 
-        $this->assertGreaterThanOrEqual($minExercises, count($exerciseIds));
-        $this->assertLessThanOrEqual($maxExercises, count($exerciseIds));
+        $this->assertGreaterThanOrEqual($min, count($result));
+        $this->assertLessThanOrEqual($max, count($result));
     }
 
     /**
-     * @return array<string, array{int, int, int}>
-     */
-    public static function provideValidRotations(): array
-    {
-        return [
-            'rotation_1' => [1, self::MIN_EXERCISES_ROTATION_1, self::MAX_EXERCISES_ROTATION_1],
-            'rotation_2' => [2, self::MIN_EXERCISES_ROTATION_2, self::MAX_EXERCISES_ROTATION_2],
-        ];
-    }
-
-    /**
-     * @group validation
      * @throws RandomException
      */
     public function testShouldThrowExceptionForInvalidRotation(): void
     {
-        $this->expectException(InvalidArgumentException::class);
-        $this->exerciseFetcher->fetchRandomExercise(5);
+        $this->expectException(\InvalidArgumentException::class);
+        $this->exerciseFetcher->fetchRandomExercise(99);
     }
 
     /**
@@ -101,80 +68,73 @@ class RandomExerciseFetcherTest extends TestCase
     {
         $this->expectException(NotEnoughExercisesException::class);
 
-        $this->repository
-            ->method('fetchByMuscleGroup')
-            ->willReturn($this->createTestExercises(MuscleGroup::CHEST, 1));
-
+        $exercise = new Exercise('Too Little', MuscleGroup::CHEST, 'desc');
+        $this->repository->insert($exercise);
         $this->exerciseFetcher->fetchRandomExercise(1);
     }
 
     /**
-     * @group rotation
      * @throws RandomException
      */
     public function testShouldProvideNonRepeatingRotationSequence(): void
     {
-        $this->repository
-            ->method('fetchByMuscleGroup')
-            ->willReturn($this->createTestExercises(MuscleGroup::CHEST, 3));
+        $this->seedExercises();
 
-        $muscleGroupRotations = [];
-
-        for ($i = 0; $i < 3; $i++) {
-            $muscleGroupRotation = $this->exerciseFetcher->getNextRotation();
-            $muscleGroupRotations[] = $muscleGroupRotation;
-            $this->exerciseFetcher->fetchRandomExercise($muscleGroupRotation);
+        $seen = [];
+        for ($i = 0; $i < 3; ++$i) {
+            $rotation = $this->exerciseFetcher->getNextRotation();
+            $seen[] = $rotation;
+            $this->exerciseFetcher->fetchRandomExercise($rotation);
         }
 
-        $this->assertCount(3, array_unique($muscleGroupRotations));
-        $this->assertEqualsCanonicalizing([1, 2, 3], $muscleGroupRotations);
-    }
-
-    public function testShouldReshuffleRotationSequenceAfterExhaustion(): void
-    {
-        $usedMuscleGroupRotations = [];
-
-        for ($i = 0; $i < 3; $i++) {
-            $usedMuscleGroupRotations[] = $this->exerciseFetcher->getNextRotation();
-        }
-
-        $reshuffledFirst = $this->exerciseFetcher->getNextRotation();
-
-        $this->assertCount(3, array_unique($usedMuscleGroupRotations));
-        $this->assertContains($reshuffledFirst, [1, 2, 3]);
-        $this->assertNotEmpty($reshuffledFirst);
+        $this->assertCount(3, array_unique($seen));
+        $this->assertEqualsCanonicalizing([1, 2, 3], $seen);
     }
 
     /**
-     * @group exercise_selection
      * @throws RandomException
      */
     public function testShouldResetExercisesWhenAllUsed(): void
     {
-        $chestExercises = $this->createTestExercises(MuscleGroup::CHEST, 2);
-        $backExercises = $this->createTestExercises(MuscleGroup::BACK, 2);
-        $armsExercises = $this->createTestExercises(MuscleGroup::ARMS, 2);
-        $shoulderExercises = $this->createTestExercises(MuscleGroup::SHOULDERS, 2);
+        $this->seedExercises(minPerGroup: 2, maxPerGroup: 2);
 
-        $this->repository
-            ->method('fetchByMuscleGroup')
-            ->willReturnCallback(function (MuscleGroup $group) use (
-                $chestExercises, $backExercises, $armsExercises, $shoulderExercises
-            ) {
-                return match ($group) {
-                    MuscleGroup::CHEST => $chestExercises,
-                    MuscleGroup::BACK => $backExercises,
-                    MuscleGroup::ARMS => $armsExercises,
-                    MuscleGroup::SHOULDERS => $shoulderExercises,
-                    default => [],
-                };
-            });
+        $first = $this->exerciseFetcher->fetchRandomExercise(2);
+        $second = $this->exerciseFetcher->fetchRandomExercise(2);
 
-        $firstRun = $this->exerciseFetcher->fetchRandomExercise(2);
-        $secondRun = $this->exerciseFetcher->fetchRandomExercise(2);
+        $this->assertCount(8, $first);
+        $this->assertCount(8, $second);
+    }
 
-        $this->assertCount(8, $firstRun);
-        $this->assertCount(8, $secondRun);
+    /** @return array[] */
+    public static function provideTestShouldReturnValidNumberOfExercisesForRotation(): array
+    {
+        return [
+            'rotation_1' => [1, 4, 6],
+            'rotation_2' => [2, 8, 12],
+        ];
+    }
+
+    public function testGetNextRotationTriggersLazyInitialization(): void
+    {
+        $reflection = new \ReflectionClass($this->exerciseFetcher);
+        $property = $reflection->getProperty('muscleGroupRotationSequence');
+        $property->setValue($this->exerciseFetcher, []);
+        $rotation = $this->exerciseFetcher->getNextRotation();
+
+        $this->assertContains($rotation, [1, 2, 3]);
+    }
+
+    /**
+     * Utility to seed exercises into the test database.
+     */
+    private function seedExercises(int $minPerGroup = 3, int $maxPerGroup = 4): void
+    {
+        foreach (MuscleGroup::cases() as $group) {
+            $count = rand($minPerGroup, $maxPerGroup);
+            for ($i = 0; $i < $count; ++$i) {
+                $exercise = new Exercise("Test $group->value $i", $group, "desc $i");
+                $this->repository->insert($exercise);
+            }
+        }
     }
 }
-
